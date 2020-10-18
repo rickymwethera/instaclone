62a96ebd9d5c03f390cf692e351894e09febf1fa
rickymwethera <rickymwethera@gmail.com>
2020-10-18 18:12:34 +0300

    initial commit

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ricky/Documents/moringa-school-projects/python-projects/instaclone/virtual/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
