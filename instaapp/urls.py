fdd53c99b56c43caf9de37fede70c5d1c40b1be3
rickymwethera <rickymwethera@gmail.com>
2020-10-27 03:22:53 +0300

    configured url patterns

--- instaapp/urls.py
+++ instaapp/urls.py
@@ -1,6 +1,13 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
-]
+    url('accounts/', include('django.contrib.auth.urls')),
+    
+] 
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
