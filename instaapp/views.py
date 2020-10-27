a9d9ff866b97cd94653fd40d749691bdffd6fd09
rickymwethera <rickymwethera@gmail.com>
2020-10-27 03:23:54 +0300

    added image display function

--- instaapp/views.py
+++ instaapp/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import Image,Comment,Profile
+
 
 # Create your views here.
 
-@login_required(login_url='accounts/login/')
+# @login_required(login_url='accounts/login/')
 
 def welcome(request):
-    return HttpResponse('THIS IS AN INSTACLONE')
+    posts=Image.objects.all()
+    return render(request, 'index.html', {"images":posts})
